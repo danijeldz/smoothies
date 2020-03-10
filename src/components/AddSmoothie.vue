76b317fe79e4ac388cf7ece0d364e46e7a666ff4
danijeldz <danijeldzankic@hotmail.com>
2020-03-10 18:12:03 +0100

    added func for storing ingredients into array on tab key press event along with empty input error handling

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="add-smoothie container">
     <h2 class="center-align indigo-text">Add new Smoothie recipe</h2>
-    <form action="" @submit.prevent="AddSmoothie">
+    <form action @submit.prevent="AddSmoothie">
       <div class="field title">
         <label for="title">Smoothie title:</label>
         <input type="text" name="title" v-model="title" />
         <div class="field add-ingredient">
           <label for="add-ingredients">Add an ingredient</label>
-          <input type="text" name="add-ingredient" />
+          <input type="text" name="add-ingredient" @keydown.tab.prevent="AddIng" v-model="another" />
         </div>
         <div class="field center-align">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
           <button class="btn pink">Add smoothie</button>
         </div>
       </div>
@@ -22,12 +23,24 @@ export default {
   name: "AddSmoothie",
   data() {
     return {
-      title: null
+      title: null,
+      another: null,
+      ingredients: [],
+      feedback: null
     };
   },
   methods: {
     AddSmoothie() {
-      console.log(this.title);
+      console.log(this.title, this.ingredients);
+    },
+    AddIng() {
+      if (this.another) {
+        this.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a valid value to add an ingredient";
+      }
     }
   }
 };
